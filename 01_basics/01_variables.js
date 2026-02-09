c553b0b3ba70bd9ff51f8e0d766017131a18a96c
Ayush pal <134642120+Ayushpal1645@users.noreply.github.com>
2026-02-09 19:10:53 +0000

    variables and const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountName = "ayush pal "
+var accountBalance = 1000
+accountcity = "delhi"
+let accountstate
+/* please use let instead of var for variable decalartion because var is function scoped and let is block scoped and 
+
+*/
+// accountId = 1
+accountName = "disha"
+accountBalance = 2000
+accountcity = "noida"
+console.log(accountId);
+console.table([accountId, accountName, accountBalance, accountcity, accountstate]);
