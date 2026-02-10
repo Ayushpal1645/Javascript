d0030e379e378952e7146a23cf18a527fbfb40bd
Ayush pal <134642120+Ayushpal1645@users.noreply.github.com>
2026-02-10 19:17:25 +0000

    operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -21,3 +21,19 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+ // **************************OPERATION*******************************
+  let value = 3;
+  let negvalue = -value;
+    console.log(negvalue);
+
+    let str1= "ayush";
+    let str2= " pal";
+    let str3 = str1 +str2;
+    console.log(str3);
+
+
+    // console.log("1"+2); //12
+    // console.log(1+"2");//12
+    // console.log("1"+2+3);//123
+    // console.log(1+2+"3");//33
