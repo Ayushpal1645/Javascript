618a99eed8af758e3c5255589c85cf074c02cebe
Ayush pal <134642120+Ayushpal1645@users.noreply.github.com>
2026-02-10 18:46:37 +0000

    Conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let score ="33";
+
+//console.log(typeof score); 
+//console.log(typeof (score));
+let valueNumber = Number(score);
+//console.log(typeof valueNumber);
+
+//33=> number
+//"33" => string
+//"33abc" => NaN (Not a Number)
+//true => 1 and false => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn); //true
+
+// 1=> true and 0 => false
+//""=> false and "Ayush" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
