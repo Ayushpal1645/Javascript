2e269049ffa359b6d483e14bf51dc8d114e1ea3c
Ayush pal <134642120+Ayushpal1645@users.noreply.github.com>
2026-02-11 21:08:27 +0000

    Stack and heap memory

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -11,7 +11,7 @@ let userEmail;
 
 const id= Symbol("123");
 const anotherID= Symbol("123");
- console.log(id===anotherID);
+ //console.log(id===anotherID);
 
 
 //non-primitive data types
@@ -26,3 +26,23 @@ let myobject={
 const myfunction=function(){
     console.log("hello ayuhs");
 }
+
+
+
+// *******************************************************************
+
+// Stack(primitve) and Heap(non-primitive)
+
+let myYoutubename="ayush";
+let anothername=myYoutubename;
+anothername="pal";
+console.log(myYoutubename);//ayush
+
+let user1={
+    email:"ayush@gmail.com",
+    upi:"ayush@okhdfc"
+
+}
+let user2=user1;
+user2.email="pal@gmail.com"
+console.log(user1.email);
