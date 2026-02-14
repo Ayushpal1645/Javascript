cd23a782143eb4bed8d09e7db5fd43dba18c6f2a
Ayush pal <134642120+Ayushpal1645@users.noreply.github.com>
2026-02-14 22:34:18 +0000

    objects2 and de structred,JSON API INTRO

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -28,3 +28,24 @@ let score1=100
 let score2=200
 let score3=300
 console.log(Array.of(score1,score2,score3));
+
+
+
+
+//******************************de-strcucture***************************** */
+
+const course={
+    coursename:"js in hindi",
+    price:999,
+    courseInstructor:"ayushpal"
+}
+const {coursename,price,courseInstructor}=course // object destructuring
+console.log(coursename);
+
+[
+  {
+    "name":"ayush",
+    "age":24,
+    "city":"delhi"
+  }
+]
